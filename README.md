5464993d262c1244643699365641b9c2d2f85a62
Andrew Ialacci <andrew@dkai.dk>
2016-02-04 23:53:33 +0100

    - Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Typed ES 6 Collections
+The type definition for [es6-collections](https://github.com/WebReflection/es6-collections).
---
+++ test/test.ts
@@ -0,0 +1,7 @@
+/// <reference path="../es6-collections.d.ts" />
+
+let map = new Map();
+let set = new Set();
+let wMap = new WeakMap();
+let sSet = new WeakSet();
+let itr: Iterator<void> = null;
